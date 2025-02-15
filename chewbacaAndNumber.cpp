75bb9bbee836f3395bf1bbdbd6cdeeff21e00471
ramkrishna mondal <ramkrishnam170@gmail.com>
2025-02-15 11:07:05 +0530

    add chewbacaAndNumber program to transform digits based on complement to 9

---
+++ chewbacaAndNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    int len=s.length();
+    string ans="";
+    for(int i=0;i<len;i++){
+        if ((9 - (s[i] - '0')) < (s[i] - '0'))
+            ans += (9 - (s[i] - '0')) + '0';
+        else ans += s[i];
+        }
+    cout<<ans<<endl;
+    return 0;
+}
