07acee520c3fbc5e878530ee5d884c7fdfe23303
ramkrishna mondal <ramkrishnam170@gmail.com>
2025-02-15 10:47:42 +0530

    implement Bit++ program to process increment and decrement operations

---
+++ Bit++.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int x=0;
+    int n;
+    cin>>n;
+    string s;
+    for(int i=0;i<n;i++){
+        cin>>s;
+        if(s[1]=='+'){
+            x++;
+        }
+        else x--;
+    }
+    cout<<x<<endl;
+    return 0;
+}
