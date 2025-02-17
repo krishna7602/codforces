65306651eddf551ec0559cf706f477e1a2e16705
ramkrishna mondal <ramkrishnam170@gmail.com>
2025-02-17 18:18:52 +0530

    add: implement prefixSumPairs program to output a sequence of 1s and 2s based on input counts

---
+++ prefixSumPairs.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int ones=0,twos=0;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        if(x==1) ones++;
+        else twos++;
+    }
+
+    if(twos==0){
+        for(int i=0;i<ones;i++){
+            cout<<"1 ";
+        }
+    }
+    else if(ones==0){
+        for(int i=0;i<twos;i++){
+            cout<<"2 ";
+        }
+    }
+    else{
+        cout<<"2 1 ";
+        for(int i=0;i<twos-1;i++) cout<<"2 ";
+        for(int i=0;i<ones-1;i++) cout<<"1 ";
+    }
+}
