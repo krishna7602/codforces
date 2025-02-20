1f5667554191164bc7490432b6954d2b02772747
ramkrishna mondal <ramkrishnam170@gmail.com>
2025-02-20 11:31:51 +0530

    add: implement madokaAndStrangeThoughts program to calculate specific outputs based on user input

---
+++ madokaAndStrangeThoughts.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<(n+(n/2)*2+(n/3)*2)<<endl;
+    }
+}
